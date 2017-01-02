f727fc4cdf167cd030736e760d03c7bb559e6723
amarmer <amarmer@users.noreply.github.com>
2017-01-02 14:38:53 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,22 @@ using namespace std;
 struct Employee : public Json::Data
 {
     Employee() {}
-    Employee(const string& name, int salary)
+    Employee(const string& firstName, const string& lastName, int salary)
     {
-        name_ = name;
+        firstName_ = firstName;
+        lastName_ = lastName;
         salary_ = salary;
     }
 
-    JSON_PROPERTY(string, name_, "name");
+    JSON_PROPERTY(string, firstName_, "firstName");
+    JSON_PROPERTY(string, lastName_, "lastName");
     JSON_PROPERTY(int, salary_, "salary");
 };
 
-struct Manager : public Employee
+struct Manager : public Json::Data
 {
-    using Employee::Employee;
-
-    JSON_PROPERTY(vector<Employee>, employees_, "employees");
+    JSON_PROPERTY(Employee, me_, "info");
+    JSON_PROPERTY(vector<Employee>, subordinates_, "subordinates");
 };
 
 struct Department : public Json::Data
@@ -37,48 +38,71 @@ struct Department : public Json::Data
 
 int main(int argc, char* argv[])
 {
-    Employee empl1("A", 50000);
-    Employee empl2("B", 55000);
+    Employee empl1("A", "K", 50000);
+    Employee empl2("B", "L", 55000);
 
-    Manager manager("X", 70000);
-    manager.employees_ = {empl1 , empl2};
+    Manager manager;
+    manager.me_ = Employee("A", "X", 70000);
+    manager.subordinates_ = {empl1 , empl2};
 
     // Change manager's last name
-    manager.name_ = "Z";
+    manager.me_.lastName_ = "Z";
 
     Department department("HR", manager);
 
     // Add employee
-    Employee employee3;
-    employee3.init("{\"name\":\"C\",\"salary\":57000}");
+    Employee empl3;
+    empl3.init("{\"firstName\":\"C\",\"lastName\":\"M\",\"salary\":57000}");
 
-    vector<Employee> employees = manager.employees_;
-    employees.push_back(employee3);
+    vector<Employee> employees = manager.subordinates_;
+    employees.push_back(empl3);
 
-    department.manager_.employees_ = employees;
+    department.manager_.subordinates_ = employees;
 
     // 'Manager' reference (when changed, 'department' data is updated)
     Manager& managerReference = department.manager_;
-    managerReference.salary_ = 75000;
+    managerReference.me_.salary_ = 75000;
 
     // 'Manager' copy (when changed, it won't affect 'department')
     Manager managerCopy = department.manager_;
-    managerCopy.salary_ = 100000;
- 
-    string str = department.toString();
+    managerCopy.me_.salary_ = 100000;
+
+    string str = department.toStyledString();
     /*
     JSON of 'department':
     {
-    "department": "HR",
-    "manager": {
-        "employees": [
-            { "name": "A", "salary": 50000}, 
-            { "name": "B", "salary": 55000}, 
-            { "name": "C", "salary": 57000}
-        ],
-        "name": "Z",
-        "salary": 75000
+        "department" : "HR",
+        "manager" : {
+            "info" : { "firstName" : "A", "lastName" : "Z", "salary" : 75000 },
+            "subordinates" : [
+                { "firstName" : "A", "lastName" : "K", "salary" : 50000 },
+                { "firstName" : "B", "lastName" : "L", "salary" : 55000 },
+                { "firstName" : "C", "lastName" : "M", "salary" : 57000 }
+            ]
         }
+    }    
+    */
+
+    struct EmployeesByFirstName : public Json::Data
+    {
+        JSON_PROPERTY(map<string COMMA vector<Employee>>, mapFirstNameEployees_, "firstNameEployees");
+    };
+
+    EmployeesByFirstName employeesByFirstName;
+    employeesByFirstName.mapFirstNameEployees_ = { {"A", {empl1, department.manager_.me_}},  {"B", {empl2}}, {"C", {empl3}} };
+
+    str = employeesByFirstName.toStyledString();
+    /*
+    JSON of 'employeesByFirstName':
+    { 
+        "firstNameEployees" : {
+            "A" : [
+                { "firstName" : "A", "lastName" : "K", "salary" : 50000 },
+                { "firstName" : "A", "lastName" : "Z", "salary" : 75000 }
+            ],
+            "B" : [ { "firstName" : "B", "lastName" : "L", "salary" : 55000 } ],
+            "C" : [ { "firstName" : "C", "lastName" : "M", "salary" : 57000 } ]
+            }
     }
     */
 
